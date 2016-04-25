61e7a52e1ebe033d4b61a7f83072f58d750c43f4
Kathryne Dunlap <kathryne@Kathrynes-MacBook-Pro.local>
2016-04-25 11:48:05 -0500

    Added time/date event input interface.

---
+++ spec/time_log_app/time_input_interface_spec.rb
@@ -0,0 +1,26 @@
+require_relative '../../time_log_app/time_input_interface'
+
+describe TimeInputInterface do
+	before :each do
+		@example = TimeInputInterface.new
+	end
+
+	it "should combine date and time input together" do
+		allow(@example).to receive(:ask_date) {"september 4th 2015"}
+		allow(@example).to receive(:ask_time) {"9:00"}
+		@example.get_time_and_date
+		expect(@example.date_and_time).to eq(DateTime.parse("september 4th 2015 9:00"))
+	end
+	
+	it "should raise an error or incorrect time input" do
+		allow(@example).to receive(:ask_time) {"Methuselah"}
+		expect { @example.get_time }.to raise_error(ArgumentError)
+	end
+
+  it "should raise an error for a date in the future" do
+    allow(@example).to receive(:ask_date) {"december 10, 2016"}
+		allow(@example).to receive(:ask_time) {"8:00"}
+		expect { @example.get_time_and_date }.to raise_error(Errors::ArgumentError)
+	end
+end
+
---
+++ time_log_app/time_input_interface.rb
@@ -0,0 +1,50 @@
+class TimeInputInterface
+
+  attr_reader :time, :date, :date_and_time
+
+	def get_time
+	  time_input = ask_time
+		verify_time_input(time_input)
+	  @time = time_input
+	end
+
+  def ask_time
+	  puts "What time?"
+		gets.chomp
+	end
+
+  def get_date
+    date_input = ask_date
+		verify_date_input(date_input)
+		@date = date_input 
+	end
+
+	def ask_date
+	  puts "What date?"
+		gets.chomp
+	end
+
+  def get_time_and_date
+    time = get_time
+		date = get_date
+  	date_and_time = (date + " " +  time) 
+  	@date_and_time = DateTime.parse(date_and_time)
+  end
+
+	def verify_time_input(input)
+    if DateTime.parse(input) 
+			input
+		else
+			raise Errors::ArgumentError.new("#{input} is an invalid time.")
+		end
+	end
+	
+	def verify_date_input(input)
+    if DateTime.parse(input) <= Date.today 
+			input
+		else
+			raise Errors::ArgumentError.new("#{input} is an invalid date.")
+		end
+	end
+
+end
