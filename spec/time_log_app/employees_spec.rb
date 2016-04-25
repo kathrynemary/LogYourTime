85811f164b2cfe8450c616e24a0b72310127ec5e
Kathryne Dunlap <kathryne@Kathrynes-MacBook-Pro.local>
2016-04-25 16:34:51 -0500

    Added error-handling to employee and client classes.

--- spec/time_log_app/employees_spec.rb
+++ spec/time_log_app/employees_spec.rb
@@ -15,6 +15,18 @@ describe Employees do
 		expect(example.list).to include('John Doe', 'Jane Doe')
 	end
 
-end
+	it "won't let you add the same employee twice." do
+    example = Employees.new
+    example.add_new_employee('John Doe')
+		expect { example.add_new_employee('John Doe') }.to raise_error(Errors::ArgumentError)
+  end
 
+	it "will raise an error if you don't verify the name " do
+    example = Employees.new
+		allow(example).to receive (:double_check) {"N"}
+    example.add_new_employee('John Doe')
+		expect { example.add_new_employee('John Doe') }.to raise_error(Errors::ArgumentError)
+  end
+
+end
 
--- time_log_app/employees.rb
+++ time_log_app/employees.rb
@@ -1,3 +1,5 @@
+require_relative 'errors'
+
 class Employees
   
   attr_reader :list
@@ -7,9 +9,26 @@ class Employees
 	end
 
   def add_new_employee(name)
-	  @list.push(name)
+	  double_check(name)
+		verify_input(name)
+		@list.push(name)
 	end
 
+  def double_check(name)
+		puts "You entered '#{name}'. Is this correct? Y/N"
+		answer = gets.upcase.chomp
+		if answer == "N"
+		  raise Errors::ArgumentError.new("Okay, never mind then!")
+		end
+	end
+
+  def verify_input(name)
+		if @list.include? name
+			raise Errors::ArgumentError.new("That name is already in our records!")
+    else
+			name
+		end
+	end
 end
 
 class Admins < Employees
