3f5142fedde3b0a7927185a1ec125a8e5172f576
Kathryne Dunlap <kathryne@Kathrynes-MacBook-Pro.local>
2016-05-12 21:43:14 -0500

    time_log_clients_totals now working

--- time_log_app/time_log_client_totals.rb
+++ time_log_app/time_log_client_totals.rb
@@ -6,11 +6,16 @@ class TimeLogClientTotals
     @client_events
 	end
 
+	def self.get_client_minutes_worked(client)
+    @client = client
+		get_client_events(@client)
+		get_event_minutes
+	end
+	
 	def self.get_client_events(client)
-		@client = client
 		get_file
-		filter_events_by_client(@client)
-    puts @client_events
+		filter_events_by_client(client)
+    get_this_months_client_events
  	end
 
 	def self.get_file
@@ -21,14 +26,10 @@ class TimeLogClientTotals
     @client_events = @file.clone
     @client_events.keys.each do |number|
       if @client_events[number]["client"] != client
-			  puts @client_events[number]["client"] #########
-				puts "that didn't match" ############
-				@client_events.delete(@client_events[number])
+				@client_events.delete(number)
       end
 		end
-	end
-	
-	
+	end	
 	
 	def self.get_this_months_client_events
     get_current_month 
@@ -40,55 +41,42 @@ class TimeLogClientTotals
 		@year = Time.now.year
 	end
 
-	def self.update_to_current_month
-    get_current_month
-		get_number_of_events
-		@updated_file = @file.clone
-		(0..@tally).each do |number|
-		  get_event_month(number)
+	def self.get_events_for_this_month
+    @client_events.keys.each do |number|
+	    get_event_month(number)
 			if @event_month != @month || @event_year != @year
-				@updated_file.delete(@updated_file.keys[number])
-			end
+				@client_events.delete(number)
+      end
 		end
-  	@updated_file
 	end
 
-  def self.get_number_of_events
-    @tally = (@file.length - 1)
-	end
-
-	def self.get_event_month(number)
-    @event = @file[@file.keys[number]]["start"]
+	def self.get_event_month(number) #also a duplicate of time_log_reader
+    @event = @client_events[number]["start"]
 		@event_month = Date.parse(@event).month
     @event_year = Date.parse(@event).year
 	end
 
-  def self.employee_totals(employee)
-		@employee = employee
-		TimeLogReader.get_events(@employee)
-	  total_hours	
-	  @sum = "#{@hours} hours and #{@minutes} minutes worked."
-	end
- 
-	def self.get_start_time
-		TimeLogReader.get_events("Jane Doe")
-    @file = TimeLogReader.updated_file
-    @start = @file[@file.keys[0]]["start"]
-	end
-
-	def self.total_hours
+  def self.get_event_minutes
     @total_minutes = 0
-		@file.keys.each do |number|
-			mins = @file[number]["minutes_worked"].to_i
+		@client_events.keys.each do |number|
+			mins = @client_events[number]["minutes_worked"].to_i
 			@total_minutes += mins 
     end
-		get_hours_from_minutes
+		get_hours_and_minutes
+	  @sum = "#{@hours} hours and #{@minutes} minutes worked."
 	end
 
-	def self.get_hours_from_minutes
+	def self.get_hours_and_minutes
     @minutes = @total_minutes % 60
 		@hours = @total_minutes / 60
 	end
 
+	def self.total_minutes
+		@total_minutes
+	end
+
+  def self.sum
+		@sum
+	end
 end
 
