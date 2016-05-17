02001729b3a49bbbabec2d68582b2e059fa2d3e0
Kathryne Dunlap <kathryne@Kathrynes-MacBook-Pro.local>
2016-05-16 20:15:58 -0500

    updated file structure

--- time_log_app.rb
+++ time_log_app.rb
@@ -5,16 +5,18 @@ require_relative 'time_log_app/employee_usernames'
 require_relative 'time_log_app/employees'
 require_relative 'time_log_app/errors'
 require_relative 'time_log_app/log_in'
+require_relative 'time_log_app/start_menu'
 require_relative 'time_log_app/log_writer'
 require_relative 'time_log_app/name_event_input_interface'
 require_relative 'time_log_app/time_input_interface'
 require_relative 'time_log_app/time_log_master'
 require_relative 'time_log_app/work_type_input_interface'
+require 'yaml/store'
 
 class TimeLogApp
 
 	def self.run
-	  LogIn.new
+	  StartMenu.new
 	end
 
 end
---
+++ time_log_app/start_menu.rb
@@ -0,0 +1,42 @@
+require_relative 'employees_list'
+
+class StartMenu
+
+  attr_reader :list_length
+	
+	def initialize
+		get_employee_list
+    evaluate_list
+	end
+
+  def evaluate_list
+		if @list_length != 0
+			log_in_menu
+		else
+			start_menu
+		end
+	end
+
+  def get_employee_list
+	  @list_length = 0	
+		(File.open("employees_list.yml")).each_line do |line|
+			@list_length += 1
+    end		
+		@list_length
+	end
+
+  def start_menu
+	  puts "Welcome! You are the first employee."	
+		puts "What is your name?"
+	  input = gets.chomp
+    Employees.new.add_new_employee(input)
+    StartMenu.new
+	end
+  
+  def log_in_menu
+		puts "Please enter your username."
+		input = gets.chomp
+    LogIn.new(input)
+	end
+
+end
