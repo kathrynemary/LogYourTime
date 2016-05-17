fd7c8f080734d4c0524ad4c9b4c153f802655186
Kathryne Dunlap <kathryne@Kathrynes-MacBook-Pro.local>
2016-05-17 15:18:33 -0500

    log_in tests passing

--- spec/time_log_app/log_in_spec.rb
+++ spec/time_log_app/log_in_spec.rb
@@ -6,18 +6,11 @@ describe LogIn do
 		EmployeeUsernames.set_up_file("example_employee_usernames.yml")
 	  EmployeesList.set_up_admin_list("example_employee_admin_list.yml")
 	end
-	
-	it "gets the employee from a username" do
-		allow(TimeLog).to receive(:non_admin_options) {3}
-		example_login = LogIn.new
-		example_login.log_in('johnnyboy')
-		expect(example_login.log_in('johnnyboy'))
-	end
 
   it "should raise an error if a non-admin selects an admin-only option." do
-		#example_login = LogIn.new
-		#allow(example_login).to receive(:employee) {"Jane Doe"}#example_login.log_in('janeiscool')
-	  expect { example_login.select_action(4)}.to raise_error(Errors::ArgumentError)
+		example_login = LogIn.new
+		allow(TimeLog).to receive(:non_admin_options) {4}
+	  expect { example_login.log_in('janeiscool') }.to raise_error(Errors::ArgumentError)
 	end
 
 end
