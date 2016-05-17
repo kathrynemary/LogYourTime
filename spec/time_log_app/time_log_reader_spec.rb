bf209838ef30d98108c9654e332780f8c104377d
Kathryne Dunlap <kathryne@Kathrynes-MacBook-Pro.local>
2016-05-16 22:31:05 -0500

    tests passint, updated to separate getting employee events and calendar events

--- spec/time_log_app/time_log_reader_spec.rb
+++ spec/time_log_app/time_log_reader_spec.rb
@@ -2,12 +2,11 @@ require 'spec_helper'
 
 describe TimeLogReader do
 
-	it "lists the events by employee" do
-	  TimeLogReader.get_events("Jane Doe")	
-		expect(TimeLogReader.tally).to eq(2)
-  end
+  before :each do
+	  TimeLogReader.get_events("Jane Doe", "example_events.yml")	
+	end
 
-  it "gets the current month" do
+	it "gets the current month" do
 		TimeLogReader.get_current_month
     expect(TimeLogReader.month).to eq(5)
 		expect(TimeLogReader.year).to eq(2016)
@@ -19,20 +18,8 @@ describe TimeLogReader do
 		expect(TimeLogReader.event_year).to eq(2016)
 	end
 	
-	it "gets the event month" do
-		TimeLogReader.get_event_month(2)
-    expect(TimeLogReader.event_month).to eq(10)
-		expect(TimeLogReader.event_year).to eq(2015)
-	end
-
-	it "checks length" do
-		TimeLogReader.get_number_of_events
-	  expect(TimeLogReader.tally).to eq(2)
+	it " " do
 	end
 
 end
 
-#TimeLogReader.get_file("Jane Doe")
-#TimeLogReader.update_to_current_month
-#puts TimeLogReader.updated_file
-#
--- time_log_app/time_log_reader.rb
+++ time_log_app/time_log_reader.rb
@@ -3,42 +3,30 @@ require 'yaml'
 
 class TimeLogReader
 
-	def self.month
-    @month
-	end
-
-  def self.year
-		@year
-	end
-	
-	def self.event_month
-		@event_month
-	end
-
-	def self.event_year
-		@event_year
-	end
-	
-	def self.print_file
-    puts @file
-	end
-
-	def self.updated_file
-    @updated_file	
-	end
-
-  def self.tally
-		@tally
-	end
-
-	def self.get_file(employee)
-    @file = YAML.load(File.open("#{employee}.yml"))
-	end
-
-  def self.get_events(employee)
+  def self.get_employee_events(employee, file="events.yml")
     @employee = employee
-		get_file(@employee)
+		get_file(file)
+    filter_by_employee
+	end
+  
+	def self.get_events(employee, file="events.yml")
+    @employee = employee
+		get_file(file)
     update_to_current_month
+	end
+
+	def self.get_file(file)
+    @file = YAML.load(File.open(file)) #filter by employee!
+		@updated_file = @file.clone
+	end
+
+	def self.filter_by_employee
+		@updated_file[:event].each do |number| 
+			unless @updated_file[:event][:employee] == @employee
+				@updated_file.delete(@updated_file.keys[number])
+			end
+		end
+		@updated_file
 	end
 
 	def self.get_current_month
@@ -48,9 +36,7 @@ class TimeLogReader
 
 	def self.update_to_current_month
     get_current_month
-		get_number_of_events
-		@updated_file = @file.clone
-		(0..@tally).each do |number| #change to @updated_file.each do |number|
+		@updated_file.each do |number| 
 		  get_event_month(number)
 			if @event_month != @month || @event_year != @year
 				@updated_file.delete(@updated_file.keys[number])
@@ -59,14 +45,26 @@ class TimeLogReader
   	@updated_file
 	end
 
-  def self.get_number_of_events
-    @tally = (@file.length - 1)
-	end
-
 	def self.get_event_month(number)
-    @event = @file[@file.keys[number]]["start"]
+    @event = @file.values.to_s
 		@event_month = Date.parse(@event).month
     @event_year = Date.parse(@event).year
+	end
+
+  def self.month
+		@month
+	end
+
+	def self.year
+		@year
+	end
+
+	def self.event_month
+		@event_month
+	end
+
+	def self.event_year
+		@event_year
 	end
 
 end
