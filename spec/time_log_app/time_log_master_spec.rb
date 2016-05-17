621817b5a358c9ed1eeeccc6403c8fd998c79cdb
Kathryne Dunlap <kathryne@Kathrynes-MacBook-Pro.local>
2016-05-16 22:57:39 -0500

    time overlap functionality not working

--- spec/time_log_app/time_log_master_spec.rb
+++ spec/time_log_app/time_log_master_spec.rb
@@ -1,32 +1,53 @@
 require 'spec_helper'
 
 describe TimeLog do
- 
- 	before :each do
-		TimeLog.new_event('Herbert Doe')
-	  TimeLog.stub_new_event
 	
-		TimeLog.new_event("Jane Doe")
-		TimeLog.stub_another_event
-	
-	end
-  
-	it "returns the employee" do
-		expect(TimeLog.get_event(:client)).to include("MegaCorp")
+	it "should throw an error if you try to submit a log for the same time twice" do
+  	TimeLog.set_up("John Doe", "example_events.yml")
+		allow(TimeLog).to receive(:client) {"MegaCorp"}  
+		allow(TimeLog).to receive(:work_type) {"billable"}  
+		allow(TimeLog).to receive(:minutes_worked) {120}  
+		allow(TimeLog).to receive(:end_time) {"may 1, 2016 10:00"}  
+		allow(TimeLog).to receive(:start_time) {"may 1, 2016 8:00"}  
+		allow(TimeLog).to receive(:employee) {"John Doe"} 	
+		TimeLog.write_to_log
+
+		TimeLog.set_up("John Doe", "example_events.yml")
+	  allow(TimeLog).to receive(:start_time) {"may 1, 2016 8:00"} 
+		allow(TimeLog).to receive(:end_time) {"may 1, 2016 10:00"} 
+		expect { TimeLog.check_times }.to raise_error(Errors::ArgumentError)
 	end
 
-	#	it "returns the client" do
-#		expect(TimeLog.get_event(:event)).to include("MegaCorp")
-#	end
+ #	before :each do
+	#	TimeLog.new_event('Herbert Doe')
+	 # TimeLog.stub_new_event
 	
+#		TimeLog.new_event("Jane Doe")
+#		TimeLog.stub_another_event
+	
+#	end
+
 #	it "returns the work type" do
 #		expect(TimeLog.get_event(:event)).to include("work type")
+#	end
+  
+#	def self.stub_another_event
+#	  @start_time = "October 3 2015, 10:30"
+#		@end_time = "October 3 2015, 12:30"
+ #   @work_type = "billable"
+	#	@client = "MegaCorp"
+   # @minutes_worked = 120
+#		write_to_log
+#	end
+
+#	def self.stub_new_event
+#	  @start_time = "April 26 2016, 9:00"
+#		@end_time = "April 26 2016, 10:00"
+ #   @work_type = "billable"
+	#	@client = "AnonyCorp"
+#    @minutes_worked = 60
+#		write_to_log
 #	end
 
 end
 
-#different files for each employee and client?
-
-#add each event to an events array, then do
-#:w
-	#	@log[:events] = @events_array
--- time_log_app/time_log_master.rb
+++ time_log_app/time_log_master.rb
@@ -1,52 +1,51 @@
+require_relative 'time_log_reader'
 require 'yaml/store'
 
 class TimeLog
 
-	attr_accessor :start_time, :end_time, :minutes_worked, :log, :event, :work_type, :client
-
-	def self.new_event(employee)
+	def self.set_up(employee, file="events.yml")
 		@employee = employee
-	  @log = YAML::Store.new("events.yml")
-	  @employee_log = YAML::Store.new("#{@employee}.yml")
+		@file = file
+	  @log = YAML::Store.new(@file)
 	end
 	
-  def self.add_new_event
-    get_start_time
+  def self.add_new_event(employee)
+    set_up(employee)	
+		get_start_time
 		get_end_time
-    calculate_time_worked
-	  get_work_type
+    check_times
+		calculate_time_worked
+		get_work_type
   	check_if_needs_client
 		write_to_log
 	end
 	
-	def self.stub_another_event
-	  @start_time = "October 3 2015, 10:30"
-		@end_time = "October 3 2015, 12:30"
-    @work_type = "billable"
-		@client = "MegaCorp"
-    @minutes_worked = 120
-		write_to_log
-	end
-
-	def self.stub_new_event
-	  @start_time = "April 26 2016, 9:00"
-		@end_time = "April 26 2016, 10:00"
-    @work_type = "billable"
-		@client = "AnonyCorp"
-    @minutes_worked = 60
-		write_to_log
-	end
-
 	def self.get_start_time
     @start_time = TimeInputInterface.new.get_time_and_date
-	end
+  end
 
 	def self.get_end_time
 		@end_time = TimeInputInterface.new.get_time_and_date
 	end
 
 	def self.calculate_time_worked
-		@minutes_worked = CalculateHours.new(@start_time, @end_time).calculate_hours.total_minutes 
+		@minutes_worked = CalculateHours.new(@start_time, @end_time).total_minutes 
+	  @minutes_worked
+	end
+
+	def self.check_times
+		@other_events = TimeLogReader.get_employee_events(@employee, @file)
+	  check_overlap
+
+  end
+
+  def self.check_overlap
+	  @other_events.each do |time|	
+			if (@start_time - time[:end]) * (time[:start] - @end_time) > 0
+			  raise Errors::ArgumentError.new("You have already logged these hours. Let's start over.")
+        add_new_event(@employee)
+			end
+		end
 	end
 
 	def self.get_work_type
@@ -61,33 +60,37 @@ class TimeLog
 		end
 	end
 
-	def self.synthesize_info
+  def self.get_client
+    puts "Which of these clients did you work for?"
+		ClientsList.display_list
+		@client = gets.chomp
+	end
+
+	def self.synthesize_event
     @event = {'employee' => @employee, 'start' => @start_time, 'end' => @end_time, 'minutes_worked' => @minutes_worked, 'work_type' => @work_type, 'client' => @client} 
 	end
 
 	def self.write_to_log
-		synthesize_info
+		unless @log
+      set_up(@employee)
+		end
+
+		synthesize_event
 		@log.transaction do
+	    @log[@employee => @start_time] ||= []
 			@log[@employee => @start_time] = @event
 			@log.commit
 		end
-		@employee_log.transaction do
-			@employee_log[@start_time] = @event
-			@employee_log.commit
-		end
 	end
 
-#Will display file in readable-ish manner.
-#  def self.get_event
-#		File.read("events.yml")  
-#		puts File.read("events.yml")
+#  def self.get_event(key)
+#    @file = FileReader.read_file(@log)
+#    puts @file.key
+#		@file.key
+#		@log.transaction do
+#		  puts @log[key]
+#		  @log[key]
+#		end
 #	end
-
-  def self.get_event(key)
-	  @log.transaction do
-		  puts @log[key]
-		  @log[key]
-		end
-	end
 
 end
