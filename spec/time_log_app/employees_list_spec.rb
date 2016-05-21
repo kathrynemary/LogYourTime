7a9189f85aa4c1720b46e7bf56627a77d8e63ad9
Kathryne Dunlap <kathryne@Kathrynes-MacBook-Pro.local>
2016-05-21 18:37:38 -0500

    updated file structure, tests passing

--- spec/time_log_app/employees_list_spec.rb
+++ spec/time_log_app/employees_list_spec.rb
@@ -3,19 +3,16 @@ require 'spec_helper'
 describe EmployeesList do
 
   it "will return list of added employees" do
-    EmployeesList.set_up_list("example_employees_list.yml")
-		#		allow(@example_employees).to receive(:ask_user_name) {"hello"}
-#		@example_employees.add_new_employee('John Doe')		
-#		allow(@example_employees).to receive(:ask_user_name) {"it's_me"}
-#		@example_employees.add_new_employee('Jane Doe') 
-	
-    expect(EmployeesList.return_list).to include('John Doe', 'Herbert Doe')
+    EmployeesList.set_up_list("spec/spec_data_files/example_employees_list_employees_list.yml")
+    EmployeesList.add_name("John Doe")
+		EmployeesList.add_name("Eugene O'Reilley")
+		expect(EmployeesList.return_list).to include('John Doe', "Eugene O'Reilley")
 	end
 
 	it "will tell you which are admins" do
-    EmployeesList.set_up_admin_list("example_admin_employees_list.yml")
-#    EmployeesList.add_admin_name("Herbert Doe")
-		expect(EmployeesList.return_admin_list).to include('Herbert Doe') 
+    EmployeesList.set_up_admin_list("spec/spec_data_files/example_employees_list_admin_employees_list.yml")
+    EmployeesList.add_admin_name("Henrietta Doe")
+		expect(EmployeesList.return_admin_list).to include('Henrietta Doe') 
 	end
 
 end
