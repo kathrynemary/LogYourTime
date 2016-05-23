d1150203b81735e982a8b3bf15db8d8a0cb96ad5
Kathryne Dunlap <kathryne@Kathrynes-MacBook-Pro.local>
2016-05-23 15:51:45 -0500

    updated tests with new file structure

--- spec/time_log_app/employees_spec.rb
+++ spec/time_log_app/employees_spec.rb
@@ -3,52 +3,34 @@ require 'spec_helper'
 describe Employees do
 
   before :each do
-		EmployeesList.set_up_list("example_employees_list.yml")
-		EmployeesList.set_up_admin_list("example_employee_admin_list.yml")
+    EmployeesList.set_up_list("spec/spec_data_files/example_employees_employees_list.yml")
+		EmployeesList.set_up_admin_list("spec/spec_data_files/example_employees_employee_admin_list.yml")
 	end
 
-	it "lets you add a new employee" do #Commenting out because test will otherwise fail when run multiple times. It will not let you add the same employee more than once.
-		example = Employees.new("example_employees_list.yml", "example_employee_admin_list.yml", "example_employee_usernames.yml")
-	  EmployeeUsernames.set_up_file("example_employee_usernames.yml")	
-#		allow(example).to receive(:ask_user_name) {'johnithan'}
-#		allow(example).to receive (:admin?) {"y"}
-		allow(example).to receive (:double_check) {"y"}
-#		example.add_new_employee('John Doe')
-
-    expect(EmployeesList.return_list).to include('John Doe')
+	it "lets you add a new employee" do 
+		example = Employees.new("spec/spec_data_files/example_employees_employees_list.yml")
+		allow(example).to receive (:double_check) {"Y"}
+		allow(example).to receive(:ask_user_name) {'johnithan'}
+		allow(example).to receive (:admin?) {"y"}
+		#example.add_new_employee('Johnithan Doe')
+    expect(EmployeesList.return_list).to include('Johnithan Doe')
 	end
 
 	it "won't let you add the same employee twice." do
 #		EmployeesList.add_name('Coldest Person')
-	  example = Employees.new("example_employees_list.yml", "example_employee_admin_list.yml") 
+	  example = Employees.new("spec/spec_data_files/example_employees_employees_list.yml") 
 #		allow(example).to receive(:ask_user_name) {'iglue'}
 #		allow(example).to receive (:admin?) {"n"}
 		allow(example).to receive (:double_check) {"y"}
 		expect { example.add_new_employee('Coldest Person') }.to raise_error(Errors::ArgumentError)
   end
 
-	it "won't let you add the same username twice." do	
-#		EmployeeUsernames.set_up_username("Coldest Person", "igloo")
-		example = Employees.new("example_employees_list.yml", "example_employee_admin_list.yml")  
-#		allow(example).to receive(:ask_user_name) {'igloo'}
-#		allow(example).to receive (:admin?) {"y"}
-		allow(example).to receive (:double_check) {"y"}
-		expect { example.add_new_employee('Less-Cold Person') }.to raise_error(Errors::ArgumentError)
-  end
-
-	it "will raise an error if you don't verify the name " do
-		example = Employees.new("example_employees_list.yml", "example_employee_admin_list.yml") 
+	it "will raise an error if you don't verify the name" do
+		example = Employees.new("spec/spec_data_files/example_employees_employees_list.yml") 
 #		allow(example).to receive(:ask_user_name) {'thejaneinspain'}
 #		allow(example).to receive (:admin?) {"n"}
 		allow(example).to receive (:double_check) {"N"}
 		expect { example.add_new_employee('John Doe') }.to raise_error(Errors::ArgumentError)
   end
-  
-	it "can add an employee to an Admins list" do
-		example = Employees.new("example_employees_list.yml", "example_employee_admin_list.yml") 
-#		example.push_admin('Herbert Doe')
-    expect(EmployeesList.return_admin_list).to include('Herbert Doe') 
-	end
-
 end
 
--- time_log_app/employees.rb
+++ time_log_app/employees.rb
@@ -1,19 +1,13 @@
-require_relative 'errors'
-require_relative 'employees_list'
-require_relative 'employee_usernames'
-require 'yaml/store'
-
 class Employees
   
-  attr_reader :list, :admins, :user_names
+  attr_reader :list
 
-	def initialize(list="employees_list.yml", admin_list="employee_admin_list.yml", user="employee_usernames.yml")
-    @list = list
-	  @admin_list = admin_list
-		@user_names = user
+	def initialize(list="employees_list.yml")
+    YAML::Store.new(list)
+		@list = list
 	end
 
-  def add_new_employee(name)
+	def add_new_employee(name)
 	  double_check(name)
 		verify_input(name)
     get_user_name(name)
@@ -22,16 +16,7 @@ class Employees
 	end
 
   def get_user_name(name)
-	  user = ask_user_name
-    double_check_user(user)
-		verify_user_name(user)
-	  EmployeeUsernames.set_up_username(name, user)
-	end
-
-  def ask_user_name
-	  puts "What would you like the username to be?"
-	  answer = gets.chomp	
-		answer
+	  EmployeeUsernames.set_up_username(name)
 	end
 
   def double_check(name)
@@ -43,15 +28,6 @@ class Employees
 		end
 	end
   
-	def double_check_user(name)
-		puts "You entered '#{name}'. Is this correct? Y/N"
-		answer = gets.upcase.chomp
-		if answer == "N"
-		  puts "Okay, never mind then!"
-		  ask_user_name
-		end
-	end
-
 	def get_employee_name
 		puts "What is the name?"
 		input = gets.chomp
@@ -65,29 +41,9 @@ class Employees
 		  end
 		end
 	end
-  
-	def verify_user_name(name)
-		if File.read(@user_names).include? name
-			raise Errors::ArgumentError.new("That username is already in our records!")
-      get_user_name(name)
-	 	else
-			name
-		end
-	end
 
 	def admin?(name)
-		puts "Would you like #{name} to be an admin? Y/N"
-		answer = gets.upcase.chomp
-		if answer == "Y"
-			push_admin(name)
-		else
-			puts "Guess not then!"
-		end
-	end
-
-	def push_admin(name)
-		puts "You answered yes."
-		EmployeesList.add_admin_name(name)
+	  EmployeeAdmins.admin?(name)
 	end
 
 end
