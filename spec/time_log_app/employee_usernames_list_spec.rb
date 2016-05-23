5b9dfc9d60669afc9802ea5a3bfa4e235b344f0f
Kathryne Dunlap <kathryne@Kathrynes-MacBook-Pro.local>
2016-05-23 14:18:00 -0500

    fixed to work with new file structure, tests passing now.

--- spec/time_log_app/employee_usernames_list_spec.rb
+++ spec/time_log_app/employee_usernames_list_spec.rb
@@ -3,21 +3,21 @@ require 'spec_helper'
 describe EmployeeUsernamesList do
 
 	it "returns a username for john" do
-		EmployeeUsernames.set_up_file("spec/spec_data_files/example_employee_usernames_employee_usernames.yml")	
-    #EmployeeUsernames.set_up_username("John Doe", "johnnyboy")
+		EmployeeUsernamesList.set_up_file("spec/spec_data_files/example_employee_usernames_employee_usernames.yml")	
+    #EmployeeUsernamesList.set_up_username("John Doe", "johnnyboy")
 
-		expect(EmployeeUsernames.username('John Doe')).to eq('johnnyboy')
+		expect(EmployeeUsernamesList.username('John Doe')).to eq('johnnyboy')
 	end
   
   it "returns an employee for a John's username" do 
-		expect(EmployeeUsernames.employee('johnnyboy')).to eq('John Doe')
+		expect(EmployeeUsernamesList.employee('johnnyboy')).to eq('John Doe')
 	end
 	
 	it "returns a username for Jane" do
-		EmployeeUsernames.set_up_file("spec/spec_data_files/example_employee_usernames_employee_usernames.yml")	
-		EmployeeUsernames.set_up_username('Jane Doe', 'janeiscool')  
+		EmployeeUsernamesList.set_up_file("spec/spec_data_files/example_employee_usernames_employee_usernames.yml")	
+		EmployeeUsernamesList.set_up_username('Jane Doe', 'janeiscool')  
 		
-		expect(EmployeeUsernames.username('Jane Doe')).to eq('janeiscool')
+		expect(EmployeeUsernamesList.username('Jane Doe')).to eq('janeiscool')
 	end
 
 end
