081b5c925d27ac6322b83acbf31a595972f36a54
Kathryne Dunlap <kathryne@Kathrynes-MacBook-Pro.local>
2016-05-23 12:07:46 -0500

    separated admin responsibility from employees.rb

---
+++ time_log_app/employee_admins.rb
@@ -0,0 +1,32 @@
+class EmployeeAdmins
+  
+	def self.set_up_list(admin_list="employee_admin_list.yml")
+    YAML::Store.new(admin_list)
+	  @admin_list = admin_list
+	end
+
+	def self.admin?(name)
+    unless @admin_list
+			set_up_list
+    end
+		interface
+	end
+
+  def self.interface
+		puts "Would you like #{name} to be an admin? Y/N"
+		answer = gets.upcase.chomp
+		if answer == "Y"
+			push_admin(name)
+		else
+			puts "Guess not then!"
+		end
+	end
+
+	def self.push_admin(name)
+		puts "You answered yes."
+		EmployeesList.add_admin_name(name)
+	end
+
+end
+
+
