d10600e78ae644fcd4bf2096095228e85625c25c
Kathryne Dunlap <kathryne@Kathrynes-MacBook-Pro.local>
2016-06-08 11:51:38 -0500

    trying to fix weird spacing

--- time_log_app/work_type_input_interface.rb
+++ time_log_app/work_type_input_interface.rb
@@ -2,44 +2,44 @@ require_relative 'errors'
 
 class WorkTypeInterface
 
-	attr_reader :timecode
+  attr_reader :timecode
 
   def get_timecode
-		input = ask_timecode
-		verify_timecode_input(input)
-		select_timecode(input)
+    input = ask_timecode
+    verify_timecode_input(input)
+    select_timecode(input)
     @timecode
-	end
-	
+  end
+
   def verify_timecode_input(input)
     if
-			input =~ /^[123]/
-		  input
-		else
-			wrong_timecode_input
-		end
-	end
+      input =~ /^[123]/
+        input
+    else
+      wrong_timecode_input
+    end
+  end
 
-	def wrong_timecode_input
-	  	raise Errors::StandardError.new
-		rescue
-			puts "That is not an option! Please try again."
-      get_timecode
-	end
+  def wrong_timecode_input
+    raise Errors::StandardError.new
+  rescue
+    puts "That is not an option! Please try again."
+    get_timecode
+  end
 
-	def ask_timecode
-	  puts "Which type of work was this? Select (1) for billable, (2) for non-billable, or (3) for paid time off."
-		gets.chomp
-	end
+  def ask_timecode
+    puts "Which type of work was this? Select (1) for billable, (2) for non-billable, or (3) for paid time off."
+    gets.chomp
+  end
 
   def select_timecode(input)
-	  if input == '1'
-			@timecode = 'billable'
-		elsif input == '2'
-			@timecode = 'non-billable'
-		elsif input == '3'
-			@timecode = 'paid time off'
-		end
-	end
+    if input == '1'
+      @timecode = 'billable'
+    elsif input == '2'
+      @timecode = 'non-billable'
+    elsif input == '3'
+      @timecode = 'paid time off'
+    end
+  end
 
 end
